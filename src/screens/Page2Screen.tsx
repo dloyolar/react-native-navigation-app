0a6d8bef3d3f093c3c5dabfba1c182bc6a07f17d
dloyolar <diego.loyola@continuumhq.com>
2022-07-25 00:01:10 -0400

    feat: add simple navigation between pages

--- src/screens/Page2Screen.tsx
+++ src/screens/Page2Screen.tsx
@@ -1,10 +1,18 @@
+import {useNavigation} from '@react-navigation/native';
 import React from 'react';
-import {View, Text} from 'react-native';
+import {View, Text, Button} from 'react-native';
+import {styles} from '../theme/appTheme';
 
 export const Page2Screen = () => {
+  const navigator = useNavigation<any>();
   return (
-    <View>
+    <View style={styles.globalMargin}>
       <Text>Page2Screen</Text>
+
+      <Button
+        title="Go to Page 3"
+        onPress={() => navigator.navigate('Page3Screen')}
+      />
     </View>
   );
 };
--- src/screens/Page3Screen.tsx
+++ src/screens/Page3Screen.tsx
@@ -1,10 +1,17 @@
 import React from 'react';
-import {View, Text} from 'react-native';
+import {View, Text, Button} from 'react-native';
+import {styles} from '../theme/appTheme';
+import {StackScreenProps} from '@react-navigation/stack';
 
-export const Page3Screen = () => {
+interface Props extends StackScreenProps<any, any> {}
+
+export const Page3Screen = ({navigation}: Props) => {
   return (
-    <View>
+    <View style={styles.globalMargin}>
       <Text>Page3Screen</Text>
+
+      <Button title="Back" onPress={() => navigation.pop()} />
+      <Button title="Back to page 1" onPress={() => navigation.popToTop()} />
     </View>
   );
 };
