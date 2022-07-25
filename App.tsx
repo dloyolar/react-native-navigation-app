eac6c9cf6f632ec99900104fdc41123ec0abc8f9
dloyolar <diego.loyola@continuumhq.com>
2022-07-24 21:10:03 -0400

    feat: add StackNavigator component and screens

--- App.tsx
+++ App.tsx
@@ -1,11 +1,12 @@
+import 'react-native-gesture-handler';
 import React from 'react';
-import {Text} from 'react-native';
 import {NavigationContainer} from '@react-navigation/native';
+import {StackNavigator} from './src/navigatior/StackNavigator';
 
 export const App = () => {
   return (
     <NavigationContainer>
-      <Text>Hola Mundo</Text>
+      <StackNavigator />
     </NavigationContainer>
   );
 };
