fabff5838cc8f364ff376b394291144e0d4b1344
dloyolar <diego.loyola@continuumhq.com>
2022-07-24 20:53:41 -0400

    chore: add react navigation

--- App.tsx
+++ App.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
-import {Text, View} from 'react-native';
+import {Text} from 'react-native';
+import {NavigationContainer} from '@react-navigation/native';
 
 export const App = () => {
   return (
-    <View>
+    <NavigationContainer>
       <Text>Hola Mundo</Text>
-    </View>
+    </NavigationContainer>
   );
 };
 
