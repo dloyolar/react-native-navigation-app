7fd335ba7bbe0cfa0979a568961770f23239cf28
dloyolar <diego.loyola@continuumhq.com>
2022-07-25 11:30:24 -0400

    feat: send data to stack screen

--- src/screens/Page1Screen.tsx
+++ src/screens/Page1Screen.tsx
@@ -1,6 +1,7 @@
+/* eslint-disable react-native/no-inline-styles */
 import {StackScreenProps} from '@react-navigation/stack';
 import React from 'react';
-import {View, Text, Button} from 'react-native';
+import {View, Text, Button, TouchableOpacity} from 'react-native';
 import {styles} from '../theme/appTheme';
 
 interface Props extends StackScreenProps<any, any> {}
@@ -13,6 +14,26 @@ export const Page1Screen = ({navigation}: Props) => {
         title="Go to Page 2"
         onPress={() => navigation.navigate('Page2Screen')}
       />
+
+      <Text>Navigate with arguments</Text>
+
+      <View style={{flexDirection: 'row'}}>
+        <TouchableOpacity
+          style={{...styles.btnBig, backgroundColor: '#5856D6'}}
+          onPress={() =>
+            navigation.navigate('PersonScreen', {id: 1, name: 'Pedro'})
+          }>
+          <Text style={styles.btnBigText}>Pedro</Text>
+        </TouchableOpacity>
+
+        <TouchableOpacity
+          style={{...styles.btnBig, backgroundColor: '#FF9427'}}
+          onPress={() =>
+            navigation.navigate('PersonScreen', {id: 2, name: 'Maria'})
+          }>
+          <Text style={styles.btnBigText}>Maria</Text>
+        </TouchableOpacity>
+      </View>
     </View>
   );
 };
