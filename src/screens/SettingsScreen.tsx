ff1334ab907acf373c4edce3d6e4fa5cbf58833e
dloyolar <diego.loyola@continuumhq.com>
2022-07-25 15:53:35 -0400

    refactor: improve styles settings screen

--- src/screens/SettingsScreen.tsx
+++ src/screens/SettingsScreen.tsx
@@ -1,10 +1,13 @@
 import React from 'react';
 import {Text, View} from 'react-native';
+import {useSafeAreaInsets} from 'react-native-safe-area-context';
+import {styles} from '../theme/appTheme';
 
 export const SettingsScreen = () => {
+  const {top} = useSafeAreaInsets();
   return (
-    <View>
-      <Text>SettingsScreen</Text>
+    <View style={{...styles.globalMargin, marginTop: top}}>
+      <Text style={styles.title}>SettingsScreen</Text>
     </View>
   );
 };
